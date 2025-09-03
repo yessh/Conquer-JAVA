a3a4ef60ce884bb2d90ed3b02fd91327f59b778d
yessh <keh1058@gmail.com>
2025-09-04 02:22:40 +0900

    생성자 개념 잡기

---
+++ src/ConstructorPractice.java
@@ -0,0 +1,20 @@
+class A {
+
+    Long id;
+    String name;
+
+    int A(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        return 0;
+    }
+    // 생성자에 반환값 적을시, 생성자가 아닌 메서드로 인식.
+}
+
+
+class B {
+
+    A a = new A();
+
+    // int A(--){--} 가 메서드로 인식되었기에, 자동으로 기본 생성자 사용.
+}
